58ed7435db7a73235f209c47b3855a1957f9488a
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-06-01 20:10:07 -0300

    Modificando o projeto para utilizar docker

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 
+const HOST = '0.0.0.0'
+const PORT = 3000
+
 const app = express();
 
 app.get('/helloworld', (req, res) => {
-    res.json({ status: 'hello world!'})
+    res.json({ status: 'hello world!!!'})
 })
 
-app.listen(3333)
+app.listen(PORT, HOST)
