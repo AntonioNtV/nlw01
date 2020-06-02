662c4206c85a72fbcca5191d1f77cda07c8bed19
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-06-02 19:17:45 -0300

    Config docker

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ const PORT = 3000
 const app = express();
 
 app.get('/helloworld', (req, res) => {
-    res.json({ status: 'hello world!!!'})
+    res.json({ status: 'hello world!!'})
 })
 
 app.listen(PORT, HOST)
