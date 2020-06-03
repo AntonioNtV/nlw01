011e3f71d7ce833fc8886b88f5fc5268ade31211
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2020-06-02 22:56:28 -0300

    minor fix

--- server/src/database/seeds/default_items.ts
+++ server/src/database/seeds/default_items.ts
@@ -7,6 +7,6 @@ export async function seed (knex: Knex) {
         { title: 'Pápeis e Papelão', image: 'papeis-papelao.svg' },
         { title: 'Resíduos Eletrônicos', image: 'elotronicos.svg' },
         { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
-        { title: 'Óleo de Cozinha', image: 'olego.svg' },
+        { title: 'Óleo de Cozinha', image: 'oleo.svg' },
     ])
 }
